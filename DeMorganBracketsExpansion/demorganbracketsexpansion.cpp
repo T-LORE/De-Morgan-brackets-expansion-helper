253814708c149184dd546a28a1d1e8d514825631
Maxim Zverkov <75385902+T-LORE@users.noreply.github.com>
2023-05-14 14:56:00 +0300

    Добавлена реализация функции получения строчки из файла

--- DeMorganBracketsExpansion/demorganbracketsexpansion.cpp
+++ DeMorganBracketsExpansion/demorganbracketsexpansion.cpp
@@ -1,6 +1,8 @@
 #include "demorganbracketsexpansion.h"
 #include <iostream>
-
+#include <QFile>
+#include <QList>
+#include <QTextStream>
 
 node *createLogicalTree(QString expression)
 {
@@ -23,8 +25,48 @@ void treeToString(node *root, QString str)
 }
 
 QString getStringFromFile(QString path)
-{
-    return 0;
+
+{   
+    QList<error> errorList;
+
+    if (path.isEmpty()) {
+        // Путь к файлу пустой
+        error pathError;
+        pathError.error = PATH_NOT_FOUND;
+        errorList.append(pathError);
+        throw errorList;
+    }
+
+    QFile file(path);
+    if (!file.exists()) {
+        // Файл по пути не найден
+        error fileError;
+        fileError.error = FILE_NOT_FOUND;
+        errorList.append(fileError);
+        throw errorList;
+    }
+
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        // Нет доступа к файлу
+        error fileError;
+        fileError.error = FILE_NOT_OPENED;
+        errorList.append(fileError);
+        throw errorList;
+    }
+
+    QTextStream in(&file);
+    QString line = in.readLine();
+
+    if (!line.isNull() && !in.atEnd()) {
+        // В файле содержится больше одной строки
+        error multipleStringsError;
+        multipleStringsError.error = MORE_THAN_ONE_STRING;
+        errorList.append(multipleStringsError);
+        throw errorList;
+    }
+
+    return line;
+
 }
 
 void saveStringToFile(QString path)
